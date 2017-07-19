b8dc39ef4c5a5d46c79e541ca99c817a03e97cc0
viswesvar <vsekar@pentaho.com>
2017-07-19 19:19:48 +0100

    [CASE STUDY] Automation test scripts for trivago.com using codeception

---
+++ vendor/bin/tests/acceptance/fifthCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class fifthCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+     $I->amOnPage('/?aDateRange%5Barr%5D=2017-08-06&aDateRange%5Bdep%5D=2017-08-07&iPathId=38715&iGeoDistanceItem=0&aCategoryRange=0%2C1%2C2%2C3%2C4%2C5&aOverallLiking=1%2C2%2C3%2C4%2C5&sOrderBy=relevance%20desc&iRoomType=7&cpt=3871503&iViewType=0&bIsSeoPage=false&bIsSitemap=false&miu=f7rdGGWoPHgY&');
+     //$I->see('Pesquisar');
+     //$I->fillField(' .horus__querytext ', 'London');
+     //$I->click(' .horus-btn-search__label ');
+     //$I->click(' .btn-horus--checkin ');
+     //$I->see('QUI');
+    }
+}
